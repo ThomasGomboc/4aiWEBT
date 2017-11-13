f5baa7f03cb0ca6e7de15eebc0d311062e8f0159
Thomas <tgomboc@gmx.at>
2017-11-13 12:04:03 +0100

    Bsp 3.

--- index.php
+++ index.php
@@ -8,3 +8,6 @@
 
 require_once 'vendor/autoload.php';
 
+$Movie = new HTL3R\KungFuMovies("Bruce Lee -Der Mann mit der Todeskralle", 5, "https://www.youtube.com/watch?v=80wXmIcyZwk", "Bruce Lee");
+
+echo isset($_GET['format']) ? $_GET['format'] : "no format given";
