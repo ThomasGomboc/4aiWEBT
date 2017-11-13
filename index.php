54af76695b1a34f9cd54b62694801febd18a525b
Thomas <tgomboc@gmx.at>
2017-11-13 12:09:48 +0100

    final commit

--- index.php
+++ index.php
@@ -8,6 +8,13 @@
 
 require_once 'vendor/autoload.php';
 
-$Movie = new HTL3R\KungFuMovies("Bruce Lee -Der Mann mit der Todeskralle", 5, "https://www.youtube.com/watch?v=80wXmIcyZwk", "Bruce Lee");
+$Movie = new HTL3R\KungFuMovies\KungFuMovieSpezial("Bruce Lee -Der Mann mit der Todeskralle", 5, "https://www.youtube.com/watch?v=80wXmIcyZwk", "Bruce Lee");
 
 echo isset($_GET['format']) ? $_GET['format'] : "no format given";
+
+function renderJSON(HTL3R\KungFuMovies\AbstractKungFuMovie $Movie): string
+{
+    json_encode($Movie);
+    return '{ "name":"fixedNameCannotAccessProperty"'.',"area":"'.$Movie->getMovieInfoAsJSON().'"}';
+
+}
